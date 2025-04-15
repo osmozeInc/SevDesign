5c5ca0b965f5caf2ee497ec3de00e98647e3e043
Caio Monte <caiomm076@gmail.com>
2025-04-15 12:09:06 -0300

    a

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 /* transição de imagens do hero section */
 
 const imagens = [
-    'url("../imagens/fundo-1.png")',
-    'url("../imagens/fundo-2.png")',
-    'url("../imagens/fundo-3.png")'
+    'url("./imagens/fundo-1.png")',
+    'url("./imagens/fundo-2.png")',
+    'url("./imagens/fundo-3.png")'
 ];
 
 const hero = document.querySelector('.hero');
